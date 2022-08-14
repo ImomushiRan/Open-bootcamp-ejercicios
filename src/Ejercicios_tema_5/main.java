ccc7027ce3d5c0e3f3add8fa6dfcf8e6e336c1fe
ImomushiRan <george.villarroel@outlook.es>
2022-08-14 01:26:26 -0300

    Segundo push (Ejercicio tema 8)

---
+++ src/Ejercicios_tema_5/main.java
@@ -0,0 +1,42 @@
+package Ejercicios_tema_5;
+
+public class main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.getEdad(25);
+        persona.getNombre("George");
+        persona.getTelefono(1166552244);
+
+        System.out.println(persona.setEdad());
+        System.out.println(persona.setNombre());
+        System.out.println(persona.setTelefono());
+    }
+
+    static class Persona {
+        private int edad;
+        private String nombre;
+        private int telefono;
+
+        public void getEdad(int edad) {
+            this.edad=edad;
+        }
+        public int setEdad() {
+            return this.edad;
+        }
+
+        public void getNombre(String nombre) {
+            this.nombre=nombre;
+        }
+        public  String setNombre() {
+            return this.nombre;
+        }
+
+        public void getTelefono(int telefono) {
+            this.telefono=telefono;
+        }
+        public int setTelefono() {
+            return this.telefono;
+        }
+    }
+}
